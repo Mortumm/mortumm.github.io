f6a36049198be954847a15679b1507646eef4074
Nuno <Mortumm@users.noreply.github.com>
2023-06-04 21:27:02 +0300

    Cookies

--- purecookie.js
+++ purecookie.js
@@ -1,6 +1,6 @@
 // --- Config --- //
 var purecookieTitle = "Cookies."; // Title
-var purecookieDesc = "By using this website, you automatically accept that we use cookies."; // Description
+var purecookieDesc = "We use those."; // Description
 var purecookieLink = '<a href="https://www.cssscript.com/privacy-policy/" target="_blank">What for?</a>'; // Cookiepolicy link
 var purecookieButton = "Understood"; // Button text
 // ---        --- //
